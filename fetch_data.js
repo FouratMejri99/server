65139c00a6d88cb9457e96451266a53af5904291
Fourat Mejri <32815591+fouratMejri@users.noreply.github.com>
2025-02-15 05:56:36 +0100

    changes

--- fetch_data.js
+++ fetch_data.js
@@ -3,7 +3,7 @@ const puppeteer = require("puppeteer");
 const tickers = ["GOOGL", "AAPL", "MSFT", "AMZN", "TSLA", "NVDA"];
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
 
   let stocksData = {};
@@ -12,7 +12,10 @@ const tickers = ["GOOGL", "AAPL", "MSFT", "AMZN", "TSLA", "NVDA"];
     const url = `https://finance.yahoo.com/quote/${ticker}`;
 
     try {
-      await page.goto(url, { waitUntil: "networkidle2" });
+      await page.goto(url, { waitUntil: "networkidle2", timeout: 60000 });
+      await page.setUserAgent(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+      );
 
       const stockData = await page.evaluate(() => {
         const getText = (selector) => {
@@ -22,7 +25,6 @@ const tickers = ["GOOGL", "AAPL", "MSFT", "AMZN", "TSLA", "NVDA"];
 
         return {
           "Current Price": getText('[data-testid="qsp-price"]'),
-
           Open: getText('[data-field="regularMarketOpen"]'),
           Volume: getText('[data-field="regularMarketVolume"]'),
           "Market Cap": getText('[data-field="marketCap"]'),
